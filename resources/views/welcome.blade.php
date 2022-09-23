a95b9746798b27f387185a526aae8f083cad9ab5
Fabian667 <cabaasfabian34734@gmail.com>
2022-09-23 00:20:53 -0300

    SE AGREGA secure_asset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/bootstrap-theme.min.css') }}">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
 
     <title>DendroMisiones</title>
 </head>
